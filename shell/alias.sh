cf4e101a5cc0545befb079d98e16c7466a202e12
Wade Bowers <wade.bowers@statenews.com>
2024-03-27 13:41:41 -0400

    fix theme remove alias i never use

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,10 +1,6 @@
 # Basic Stuff
 alias ls="ls --color"
-alias edit="code"
-alias subl="subl"
 alias src="source ~/.zshrc"
-alias open.=" open ."
-alias cl="clear"
 alias cp="rsync -ahv --progress"
 alias filecount="ls -l | wc -l"
 
@@ -19,20 +15,13 @@ alias lanip="ifconfig | grep 'inet'"
 alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias mkhttp="python -m http.server"
 
-# Compiling
-alias compA='g++ -g -Wall -std=c++11 *.cpp *.h'
-alias comp='g++ -g -Wall -std=c++11'
-
 # Misc
 alias ytdlmp3="youtube-dl --add-metadata -x --audio-format 'mp3' --audio-quality 0"
 alias mpv="mpv --no-border"
 alias mpvpip="mpv --ontop --no-border --on-all-workspaces --autofit=384x216 --geometry=99%:2%"
 
-
+alias btop="btop -t"
 alias bmon="bmon -f /Users/wade/.config/bmon.conf"
-alias limelight="limelight &> /dev/null &"
-
-
 
 # macOS Show/Hide Hidden Files
 alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder"
